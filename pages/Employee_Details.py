916ea09ea68ddf2ed473029f494c315dfb7226b5
Andrew <andrew.barwise@westernpower.com.au>
2022-09-08 13:18:51 +0800

    changes to employee_details page

--- pages/Employee_Details.py
+++ pages/Employee_Details.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 
 
-st.session_state.employees_df = pd.read_csv('data\employees.csv')
+st.session_state.employees_df = pd.read_csv('data/employees.csv')
+st.session_state.employees_df.reset_index(drop=True, inplace=True)
 
 st.title("Employee Details")
 st.write("The purpose of this page is to allow admin/ supervisor to be able \
@@ -21,13 +22,13 @@ depot = st.selectbox('Select the depot',('Perth','Bunbury'))
 
 if st.button('Update'):
     if option == 'Add Employee':
-        new_entry = pd.Series(data={'Name':name, 'Depot':depot,'Position':'Controller'}, name = 'x')
-        st.session_state.employees_df = st.session_state.employees_df.append(new_entry, ignore_index=True)
+        new_entry = [{'Name':name, 'Depot':depot,'Position':'Controller'}]
+        st.session_state.employees_df = st.session_state.employees_df.append(new_entry)
 
     else:
         st.session_state.employees_df.drop(st.session_state.employees_df[st.session_state.employees_df['Name'] == name].index, inplace = True)
     
     
-    st.session_state.employees_df.to_csv('data/employees.csv')
+    st.session_state.employees_df.to_csv('data/employees.csv',index=False)
 
 
--- pages/Shift_Unavailability.py
+++ pages/Shift_Unavailability.py
@@ -1,20 +1,31 @@
 import streamlit as st
 import pandas as pd
 
-@st.cache(allow_output_mutation=True)
-def get_data():
-    return []
+st.session_state.availability_df = pd.read_csv('data/availability.csv')
+st.session_state.availability_df.reset_index(drop=True, inplace=True)
+
+st.title("Employee Details")
+st.write("The purpose of this page is to allow admin/ supervisor to be able \
+    to update leave requests and save the updates to the csv file")
+
+st.subheader("Current Leave Requests")
+if st.checkbox("Show Leave Requests"):
+    st.subheader("Leave Requests")
+    st.table(st.session_state.availability_df)
 
 
-emp_list = st.selectbox("Employee Name",
-('James','Molly','Sam','Melissa'))
+# this needs to be a list from the employees csv file
+emp_list = st.selectbox("Employee Name",('James','Molly','Sam','Melissa'))
 
 date = st.date_input("Select Date")
 
 shift = st.radio("Please select your shift",
 ('Day','Night'))
 
-if st.button('Update'):
-    get_data().append({'Employee':emp_list,'Date':date,'Shift':shift})
 
-st.write(pd.DataFrame(get_data()))
+st.subheader('Edit the employees dataframe')
+
+if st.button('Update'):
+    new_entry = [{'Employee':emp_list, 'Date':date,'Shift':shift}]
+    st.session_state.availability_df = st.session_state.availability_df.append(new_entry)
+    st.session_state.availability_df.to_csv('data/availability.csv',index=False)
