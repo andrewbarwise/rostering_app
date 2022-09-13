cd8283f677f178d3f78b2136d07ee7d5feac356f
Andrew <andrew.barwise@westernpower.com.au>
2022-09-13 15:50:51 +0800

    changes to shift availability and shift setup

--- pages/Shift_Setup.py
+++ pages/Shift_Setup.py
@@ -3,10 +3,27 @@ import streamlit as st
 import pandas as pd
 from datetime import timedelta
 
+# bring in the employees data as we need the 'Name' column to use as an index in the shift_df dataframe
+st.session_state.employees_df = pd.read_csv('data/employees.csv')
+st.session_state.employees_df.reset_index(drop=True, inplace=True)
+
+names = st.session_state.employees_df['Name'].values.tolist()
+
+st.title("Shift Setup Details")
+st.write("The purpose of this page is to allow admin/ supervisor to be able \
+    to remove or add employees and save the updates to the csv file")
+
 min_date = datetime.date(datetime.now())
 max_date =  min_date + timedelta(days=365)
 date_range = st.date_input("Select the date range",(min_date, max_date))
 
 days = pd.date_range(date_range[0], date_range[1], freq='d').to_list()
 
-st.write(days)
+st.session_state.shift_df = pd.DataFrame(columns=days,index=names)
+st.session_state.shift_df.iloc[:] = 1
+
+st.subheader("Current shift setup data")
+if st.checkbox("Show shift setup data"):
+    st.subheader("Shifts")
+    st.table(st.session_state.shift_df)
+
--- pages/Shift_Unavailability.py
+++ pages/Shift_Unavailability.py
@@ -4,7 +4,13 @@ import pandas as pd
 st.session_state.availability_df = pd.read_csv('data/availability.csv')
 st.session_state.availability_df.reset_index(drop=True, inplace=True)
 
-st.title("Employee Details")
+# bring in the employees data as we need the 'Name' column to use as an index in the shift_df dataframe
+st.session_state.employees_df = pd.read_csv('data/employees.csv')
+st.session_state.employees_df.reset_index(drop=True, inplace=True)
+
+names = st.session_state.employees_df['Name'].values.tolist()
+
+st.title("Leave Requests")
 st.write("The purpose of this page is to allow admin/ supervisor to be able \
     to update leave requests and save the updates to the csv file")
 
@@ -15,17 +21,17 @@ if st.checkbox("Show Leave Requests"):
 
 
 # this needs to be a list from the employees csv file
-emp_list = st.selectbox("Employee Name",('James','Molly','Sam','Melissa'))
+emp_list = st.selectbox("Employee Name",names)
 
 date = st.date_input("Select Date")
 
-shift = st.radio("Please select your shift",
-('Day','Night'))
+#shift = st.radio("Please select your shift",
+#('Day','Night'))
 
 
 st.subheader('Edit the employees dataframe')
 
 if st.button('Update'):
-    new_entry = [{'Employee':emp_list, 'Date':date,'Shift':shift}]
+    new_entry = [{'Employee':emp_list, 'Date':date}]
     st.session_state.availability_df = st.session_state.availability_df.append(new_entry)
     st.session_state.availability_df.to_csv('data/availability.csv',index=False)
